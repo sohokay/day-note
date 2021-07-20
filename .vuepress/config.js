bce8999ddbbcc2fcbb13f461be4c023ec248d502
fengkch <fengkch@dingtalk.com>
2021-07-20 20:26:24 +0800

    edit

---
+++ .vuepress/config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  themeConfig: {
+    displayAllHeaders: true, // 默认值：false
+    lastUpdated: '最后更新于', // string | boolean
+    repo: 'https://github.com/sohokay/day-note',
+    repoLabel: '查看源码',
+    docsBranch: 'main',
+    editLinks: true,
+    // 默认为 "Edit this page"
+    editLinkText: '帮助我们改善此页面！',
+
+
+    theme: '@vuepress/blog',
+    themeConfig: {
+      // 请参考文档来查看所有可用的选项。
+    },
+
+
+    nav: [ // 导航栏
+      {text: 'Home', link: '/'},
+      {text: 'Guide', link: '/guide/'},
+      {text: 'Git', link: '/git/'},
+      {text: 'Note', link: '/note/'},
+      {
+        text: 'Language', link: '/guide/', items: [
+          {text: 'Chinese', link: '/language/chinese/'},
+          {text: 'Japanese', link: '/language/japanese/'}
+        ]
+      },
+
+    ],
+    sidebar: 'auto'
+  }
+}
